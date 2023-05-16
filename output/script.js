561a71e89f6daec1a5915b159cf26d5a8742565d
Lux <web-lux@outlook.fr>
2023-05-16 21:42:47 +0200

    ref: modifications de certains typages et fusion des event listeners à classList.toggle

--- output/script.js
+++ output/script.js
@@ -24,31 +24,35 @@ function getUserInput() {
     return userInput;
 }
 function createGridFromInput() {
-    let userInput = getUserInput();
+    let userInput = getUserInput(); //getUserInput fait office de validateur
     if (userInput) {
+        // s'il ne renvoie pas undefined, renderGrid peut se lancer
         renderGrid(userInput);
         [...canvasEl.children].forEach((div) => {
-            div.addEventListener("mouseover", handleHover);
-            div.addEventListener("click", handleClick);
+            /* le spread operator permet d'itérer sur la collection HTML comme sur un array */
+            div.addEventListener("mouseover", handleHover); // mouseover gère le pen et l'eraser
+            div.addEventListener("click", handleClick); // click gère le bucket et le picker
         });
     }
 }
 function renderGrid(size) {
+    // la fonction crée un array d'élement div en fonction de la valeur de sizeInput
     let result = [];
     for (let i = 0; i < size * size; i++) {
         result.push(document.createElement("div"));
     }
     canvasEl.replaceChildren(...result);
-    canvasEl.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+    canvasEl.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`; // modifie le CSS de la grid en fonction de la valeur de sizeInput
 }
 function rgbToHex(rgb) {
+    // fonction utilisée par le bouton colorPicker et lorsqu'on sélectionne une couleur prédéfinie de la palette
     let regexResult = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
     let result = regexResult === null || regexResult === void 0 ? void 0 : regexResult.slice(1).map((n) => parseInt(n, 10).toString(16).padStart(2, "0")).join("");
     return `#${result}`;
 }
 function handleHover() {
     if (penBtn.classList.contains("activated")) {
-        this.style.backgroundColor = randomBtn.classList.contains("activated")
+        this.style.backgroundColor = randomBtn.classList.contains("activated") // vérifie si le bouton random est activé ou non et adapte la couleur en conséquence
             ? getRandomHex()
             : colorInput.value;
     }
@@ -61,7 +65,7 @@ function handleHover() {
 function handleClick() {
     if (bucketBtn.classList.contains("activated")) {
         [...canvasEl.children].forEach((div) => {
-            div.style.backgroundColor = randomBtn.classList.contains("activated")
+            div.style.backgroundColor = randomBtn.classList.contains("activated") // vérifie si le bouton random est activé ou non et adapte la couleur en conséquence
                 ? getRandomHex()
                 : colorInput.value;
         });
@@ -79,6 +83,7 @@ function displayGrid() {
     canvasEl.classList.toggle("grid");
 }
 function getRandomHex() {
+    // utilisée pour le pen et le bucket
     const possibleCharacters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
@@ -88,7 +93,9 @@ function getRandomHex() {
     return color;
 }
 function classToggle(element) {
+    // utilisée par les event listeners
     if (element.parentNode.id == "toolbox") {
+        // dans le cas où l'élement cliqué a la toolbox comme container parent, empêche d'avoir plusieurs outils sélectionnés en même temps via un reset
         [...toolboxEl.children].forEach((el) => {
             el.classList.remove("activated");
         });
@@ -97,6 +104,8 @@ function classToggle(element) {
 }
 /* EVENT LISTENER */
 paletteColorEls.forEach((div) => {
+    // itère sur tous les élements de la palette pour que cliquer
+    // sur l'un d'eux remplace la valeur de colorInput par la valeur de la couleur du fond
     div.addEventListener("click", () => {
         colorInput.value = rgbToHex(div.style.backgroundColor);
     });
@@ -106,23 +115,14 @@ sizeInputEl.addEventListener("change", function () {
 });
 clearBtn.addEventListener("click", clearCanvas);
 gridBtn.addEventListener("click", (event) => {
-    classToggle(event.target);
     displayGrid();
 });
-randomBtn.addEventListener("click", (event) => {
-    classToggle(event.target);
-});
-penBtn.addEventListener("click", (event) => {
-    classToggle(event.target);
-});
-eraserBtn.addEventListener("click", (event) => {
-    classToggle(event.target);
-});
-bucketBtn.addEventListener("click", (event) => {
-    classToggle(event.target);
-});
-pickerBtn.addEventListener("click", (event) => {
-    classToggle(event.target);
+[randomBtn, penBtn, eraserBtn, bucketBtn, pickerBtn, gridBtn].forEach(
+// pour être plus DRY, la fonction réunit tous les boutons activables (et surtout, qui restent "pressés" via css) dans un array
+(element) => {
+    element.addEventListener("click", (event) => {
+        classToggle(event.target);
+    });
 });
 /* INITIALISATION */
 createGridFromInput();
--- script.ts
+++ script.ts
@@ -7,7 +7,9 @@ const sizeInputEl = document.querySelector(
 const colorInput = document.querySelector(
 	"#color-picker-container input"
 ) as HTMLInputElement;
-const paletteColorEls = document.querySelectorAll("#palette div");
+const paletteColorEls = document.querySelectorAll(
+	"#palette div"
+) as NodeListOf<Element>;
 const clearBtn = document.querySelector(
 	"button#clear-btn"
 ) as HTMLButtonElement;
@@ -35,37 +37,42 @@ function getUserInput(): number | undefined {
 }
 
 function createGridFromInput() {
-	let userInput = getUserInput();
+	let userInput = getUserInput(); //getUserInput fait office de validateur
 	if (userInput) {
+		// s'il ne renvoie pas undefined, renderGrid peut se lancer
 		renderGrid(userInput);
 		[...canvasEl.children].forEach((div) => {
-			div.addEventListener("mouseover", handleHover);
-			div.addEventListener("click", handleClick);
+			/* le spread operator permet d'itérer sur la collection HTML comme sur un array */
+
+			div.addEventListener("mouseover", handleHover); // mouseover gère le pen et l'eraser
+			div.addEventListener("click", handleClick); // click gère le bucket et le picker
 		});
 	}
 }
 
 function renderGrid(size: number) {
+	// la fonction crée un array d'élement div en fonction de la valeur de sizeInput
 	let result = [];
 	for (let i = 0; i < size * size; i++) {
 		result.push(document.createElement("div"));
 	}
 	canvasEl.replaceChildren(...result);
-	canvasEl.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+	canvasEl.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`; // modifie le CSS de la grid en fonction de la valeur de sizeInput
 }
 
-function rgbToHex(rgb: string) {
+function rgbToHex(rgb: string): string {
+	// fonction utilisée par le bouton colorPicker et lorsqu'on sélectionne une couleur prédéfinie de la palette
 	let regexResult = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
 	let result = regexResult
-		?.slice(1)
+		?.slice(1) // slice à partir de 1 car le premier élement est "rgb([valeur du rgb])"
 		.map((n) => parseInt(n, 10).toString(16).padStart(2, "0"))
 		.join("");
 	return `#${result}`;
 }
 
-function handleHover(this: any) {
+function handleHover(this: HTMLElement) {
 	if (penBtn.classList.contains("activated")) {
-		this.style.backgroundColor = randomBtn.classList.contains("activated")
+		this.style.backgroundColor = randomBtn.classList.contains("activated") // vérifie si le bouton random est activé ou non et adapte la couleur en conséquence
 			? getRandomHex()
 			: colorInput.value;
 	} else if (eraserBtn.classList.contains("activated")) {
@@ -73,10 +80,10 @@ function handleHover(this: any) {
 	} else return;
 }
 
-function handleClick(this: any) {
+function handleClick(this: HTMLElement) {
 	if (bucketBtn.classList.contains("activated")) {
 		[...canvasEl.children].forEach((div: any) => {
-			div.style.backgroundColor = randomBtn.classList.contains("activated")
+			div.style.backgroundColor = randomBtn.classList.contains("activated") // vérifie si le bouton random est activé ou non et adapte la couleur en conséquence
 				? getRandomHex()
 				: colorInput.value;
 		});
@@ -96,6 +103,7 @@ function displayGrid() {
 }
 
 function getRandomHex(): string {
+	// utilisée pour le pen et le bucket
 	const possibleCharacters = "0123456789ABCDEF";
 	let color = "#";
 
@@ -108,7 +116,9 @@ function getRandomHex(): string {
 }
 
 function classToggle(element: any) {
+	// utilisée par les event listeners
 	if (element.parentNode.id == "toolbox") {
+		// dans le cas où l'élement cliqué a la toolbox comme container parent, empêche d'avoir plusieurs outils sélectionnés en même temps via un reset
 		[...toolboxEl.children].forEach((el) => {
 			el.classList.remove("activated");
 		});
@@ -119,6 +129,8 @@ function classToggle(element: any) {
 /* EVENT LISTENER */
 
 paletteColorEls.forEach((div: any) => {
+	// itère sur tous les élements de la palette pour que cliquer
+	// sur l'un d'eux remplace la valeur de colorInput par la valeur de la couleur du fond
 	div.addEventListener("click", () => {
 		colorInput.value = rgbToHex(div.style.backgroundColor);
 	});
@@ -131,29 +143,17 @@ sizeInputEl.addEventListener("change", function () {
 clearBtn.addEventListener("click", clearCanvas);
 
 gridBtn.addEventListener("click", (event) => {
-	classToggle(event.target);
 	displayGrid();
 });
 
-randomBtn.addEventListener("click", (event) => {
-	classToggle(event.target);
-});
-
-penBtn.addEventListener("click", (event) => {
-	classToggle(event.target);
-});
-
-eraserBtn.addEventListener("click", (event) => {
-	classToggle(event.target);
-});
-
-bucketBtn.addEventListener("click", (event) => {
-	classToggle(event.target);
-});
-
-pickerBtn.addEventListener("click", (event) => {
-	classToggle(event.target);
-});
+[randomBtn, penBtn, eraserBtn, bucketBtn, pickerBtn, gridBtn].forEach(
+	// pour être plus DRY, la fonction réunit tous les boutons activables (et surtout, qui restent "pressés" via css) dans un array
+	(element) => {
+		element.addEventListener("click", (event) => {
+			classToggle(event.target);
+		});
+	}
+);
 
 /* INITIALISATION */
 
